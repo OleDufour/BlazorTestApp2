d92ac97539df0a23a4d1fdea0a3f541b0182c647
ODufour <oledufour@hotmail.com>
2020-05-10 09:52:09 +0200

    Syncfusion.Blazor working !

--- BlazorAppMysql/Client/Program.cs
+++ BlazorAppMysql/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 
 namespace BlazorAppMysql.Client
 {
@@ -14,11 +15,13 @@ namespace BlazorAppMysql.Client
     {
         public static async Task Main(string[] args)
         {
+            // 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddSyncfusionBlazor();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjU0NTI4QDMxMzgyZTMxMmUzMGkzZmxGN2hDS1hhdjV6NmdDSnpMRzRqbEhrdGw1OHY0U2dIbG5iaWVmY289");
             await builder.Build().RunAsync();
         }
     }
---
+++ BlazorAppMysql/Server/Controllers/VoteController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlazorAppMysql.Server.DtoModels;
+
+using BlazorAppMysql.Server;
+using BlazorAppMysql.Shared;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorAppMysql.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class VoteController : ControllerBase
+    {
+        private readonly PropositionVoterContext _context;
+
+        public VoteController(PropositionVoterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public List<Vote> GetPropositions()
+        {
+            IQueryable<Vote> v = _context.Vote.Select(x => x);
+            return v.ToList();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        [HttpPost]
+        //[Route("Create")]
+        public async Task<IActionResult> Add(Vote proposition)
+        {
+            var response = new ResponseSingle<int>();
+            _context.Add(proposition);
+            await _context.SaveChangesAsync();
+            //      return NoContent();
+            return Ok(response);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
